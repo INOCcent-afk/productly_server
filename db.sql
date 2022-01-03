4d8a8b6f39bce5250ca53549524ee1feb92095bb
Dave Inoc <michael828inoc@gmail.com>
2022-01-03 23:34:17 +0800

    dance the night away

---
+++ db.sql
@@ -0,0 +1,146 @@
+CREATE DATABASE productly_db;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users(
+    user_id UUID DEFAULT uuid_generate_v4(),
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    cover_photo TEXT,
+    display_picture TEXT,
+    PRIMARY KEY(user_id)
+);
+
+CREATE TABLE products(
+    product_id BIGSERIAL NOT NULL,
+    user_id UUID NOT NULL,
+    product_name VARCHAR(150) NOT NULL,
+    product_description TEXT NOT NULL,
+    product_image VARCHAR(250) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    PRIMARY KEY(product_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE reviews(
+    review_id BIGSERIAL NOT NULL,
+    user_id UUID NOT NULL,
+    reviews_product_id BIGINT NOT NULL,
+    review_message TEXT,
+    rating INT NOT NULL check(rating >= 1 and rating <=5 ),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    PRIMARY KEY(review_id),
+    FOREIGN KEY (reviews_product_id) REFERENCES products(product_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+INSERT INTO users(first_name, last_name, email, password) values('dave', 'Inoc', 'tangina@gmail.com', 'rachelle');
+
+INSERT INTO products(user_id, product_name, product_description, product_image) values('166e0efb-ee3f-47ab-b8c9-5c9b74187ec4', 'Nescafe', '100% pure soluble instant coffee. With NESCAFE Classic, you prepare every cup with real coffee flavor. Made with only high quality beans, every cup is a guarantee that youll get great coffee taste and superb aroma no matter how you mix it.', '');
+
+INSERT INTO products(user_id, product_name, product_description, product_image) values('166e0efb-ee3f-47ab-b8c9-5c9b74187ec4', 'Zibra', 'Best Burger Refreshing!', 'https://images.pexels.com/photos/341523/pexels-photo-341523.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+
+INSERT INTO reviews(reviews_product_id, user_id, review_message, rating) values('2', '166e0efb-ee3f-47ab-b8c9-5c9b74187ec4', 'quite sweet it probably helps the flavor', 5);
+INSERT INTO reviews(reviews_product_id, user_id, review_message, rating) values('2', '166e0efb-ee3f-47ab-b8c9-5c9b74187ec4', 'Very refreshing indeed', 5);
+
+"166e0efb-ee3f-47ab-b8c9-5c9b74187ec4"
+
+SELECT 
+    users.user_id,
+    display_name, 
+    email, 
+    cover_photo, 
+    display_picture,
+    product_id, 
+    product_name,
+    product_description
+FROM 
+    users 
+    LEFT JOIN products ON users.user_id = products.user_id; 
+
+SELECT count(rating) FROM reviews WHERE user_id = '166e0efb-ee3f-47ab-b8c9-5c9b74187ec4';
+SELECT count(review_message) FROM reviews WHERE user_id = '166e0efb-ee3f-47ab-b8c9-5c9b74187ec4';
+
+SELECT TRUNC(AVG(rating),2) AS average_rating FROM reviews WHERE product_id = 2;
+SELECT TRUNC(AVG(rating),2) AS average_rating FROM reviews GROUP BY product_id;
+
+SELECT * FROM products LEFT JOIN (SELECT reviews_product_id, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews GROUP BY reviews_product_id) reviews on products.product_id = reviews.reviews_product_id;
+
+-- Multipage Pagination 
+SELECT 
+    (SELECT COUNT(*) FROM users) as count, 
+    (SELECT json_agg(t.*) FROM (
+        SELECT * FROM users
+        LIMIT 2 OFFSET (1 - 1) * 2
+    ) AS t) AS rows;
+
+-- start of next prev pagination
+
+-- Create Index
+CREATE UNIQUE INDEX index_id ON users USING btree(user_id ASC);
+
+-- 1st Page
+SELECT * FROM users ORDER BY user_id ASC LIMIT 2;
+
+-- 2nd Page
+SELECT * FROM users WHERE user_id > '7f82dd81-0b9f-4011-ad80-0a4824a3cf3d' ORDER BY user_id ASC LIMIT 2;
+
+-- end of next prev pagination
+
+select * from users where lower(first_name) || ' ' || lower(last_name)  'D';
+
+UPDATE users SET 
+          first_name = COALESCE('', first_name), 
+          last_name = COALESCE('', last_name), 
+          bio_description = COALESCE('', bio_description), 
+          display_picture = COALESCE(null, display_picture), 
+          cover_photo = COALESCE(null, cover_photo) 
+        
+        WHERE user_id = '7f82dd81-0b9f-4011-ad80-0a4824a3cf3d'
+        AND ( coalesce('', '') = '' AND '' IS DISTINCT FROM first_name OR 
+              coalesce('', '') = '' AND '' IS DISTINCT FROM last_name OR 
+              coalesce('', '') = '' AND '' IS DISTINCT FROM bio_description OR 
+              coalesce(null, '') = '' AND null IS DISTINCT FROM display_picture OR 
+              coalesce(null, '') = '' AND null IS DISTINCT FROM cover_photo ) returning *;
+              
+select count(reviews_product_id) from reviews where reviews_product_id = '2' and review_message <> '';
+
+SELECT product_name, product_description, product_image, count, average_rating, latest_review_date, product_id, user_id
+    FROM products INNER JOIN 
+    (SELECT reviews_product_id, MAX(created_at) AS latest_review_date, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews WHERE reviews.user_id = '339d1daa-2f66-46b7-b7bd-bdd980039185' GROUP BY reviews_product_id) 
+        reviews 
+        on products.product_id = reviews.reviews_product_id;
+
+'339d1daa-2f66-46b7-b7bd-bdd980039185'
+
+UPDATE users SET 
+        first_name = COALESCE('maytmar', first_name), 
+        last_name = COALESCE('inoc', last_name), 
+        bio_description = COALESCE('nice', bio_description), 
+        display_picture = COALESCE(null, display_picture), 
+        cover_photo = COALESCE(null, cover_photo) 
+
+        WHERE user_id = 'ef087746-f7b8-48c1-9610-b136acfb6cb3'
+        AND ( ('maytmar'::VARCHAR(255) <> '') IS NOT TRUE AND 'maytmar' IS DISTINCT FROM first_name OR 
+                ('inoc'::VARCHAR(255) <> '') IS NOT TRUE AND 'inoc' IS DISTINCT FROM last_name OR 
+                ('nice'::TEXT <> '') IS NOT TRUE AND 'nice' IS DISTINCT FROM bio_description OR 
+                (''::TEXT <> '') IS NOT TRUE AND '' IS DISTINCT FROM display_picture OR 
+                (''::TEXT <> '') IS NOT TRUE AND '' IS DISTINCT FROM cover_photo ) returning *;
+
+    UPDATE users SET 
+        first_name = COALESCE('maytmar', first_name), 
+        last_name = COALESCE('inoc', last_name), 
+        bio_description = COALESCE(null, bio_description), 
+        display_picture = COALESCE(null, display_picture), 
+        cover_photo = COALESCE(null, cover_photo) 
+
+        WHERE user_id = 'ef087746-f7b8-48c1-9610-b136acfb6cb3';
+
+        SELECT * FROM products
+            LEFT JOIN 
+                ( SELECT reviews_product_id, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews GROUP BY reviews_product_id ) 
+                    reviews 
+                on products.product_id = reviews.reviews_product_id
+                WHERE LOWER(product_name) ~* 'z';
---
+++ server.js
@@ -0,0 +1,533 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+const validInfo = require("./middlewares/validInfo");
+const authorize = require("./middlewares/authorize");
+const jwtGenerator = require("./utils/jwtGenerator");
+const bcrypt = require("bcrypt");
+const { cloudinary } = require("./utils/cloudinary.js");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+// Get all users
+app.get("/api/v1/productly/users", async (req, res) => {
+  try {
+    const users = await db.query("SELECT * FROM users");
+
+    res.status(200).json({
+      status: "success",
+      results: users.rows.length,
+      data: {
+        users: users.rows,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// Get single user
+app.get("/api/v1/productly/user/:id", async (req, res) => {
+  try {
+    const user = await db.query(
+      "SELECT user_id, first_name, last_name, email, cover_photo, display_picture, bio_description FROM users WHERE user_id = $1;",
+      [req.params.id]
+    );
+
+    const userReviewsCount = await db.query(
+      "select count(user_id) from reviews where user_id = $1 and review_message <> '';",
+      [req.params.id]
+    );
+
+    const userRatingsCount = await db.query(
+      "SELECT count(rating) FROM reviews WHERE user_id = $1;",
+      [req.params.id]
+    );
+
+    const activity = await db.query(
+      `SELECT product_name, product_description, product_image, count, average_rating, latest_review_date, product_id, user_id
+      FROM products INNER JOIN 
+      (SELECT reviews_product_id, MAX(created_at) AS latest_review_date, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews WHERE reviews.user_id = $1 GROUP BY reviews_product_id) 
+          reviews 
+          on products.product_id = reviews.reviews_product_id;`,
+      [req.params.id]
+    );
+
+    res.status(200).json({
+      status: "success",
+      user_results: user.rows.length,
+      data: {
+        user: user.rows[0],
+        user_activity: {
+          activity: activity.rows,
+        },
+        userReviewsCount: userReviewsCount.rows[0],
+        userRatingsCount: userRatingsCount.rows[0],
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// Search single user
+app.get("/api/v1/productly/user/search/:name", async (req, res) => {
+  try {
+    const users = await db.query(
+      "SELECT user_id, first_name, last_name, display_picture FROM users WHERE LOWER(first_name) || ' ' || LOWER(last_name) ~* $1",
+      [`^${req.params.name}.*`]
+    );
+
+    res.status(200).json({
+      status: "success",
+      user_results: users.rows.length,
+      data: {
+        users: users.rows,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// Sign up
+app.post("/api/v1/productly/signup", validInfo, async (req, res) => {
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    cover_photo,
+    display_picture,
+  } = req.body;
+
+  try {
+    const user = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (user.rows.length > 0) {
+      return res.status(401).json("User already exist!");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const bcryptPassword = await bcrypt.hash(password, salt);
+
+    let newUser = await db.query(
+      "INSERT INTO users (first_name, last_name, email, password, cover_photo, display_picture) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [
+        first_name,
+        last_name,
+        email,
+        bcryptPassword,
+        cover_photo,
+        display_picture,
+      ]
+    );
+
+    const jwtToken = jwtGenerator(newUser.rows[0].user_id);
+
+    return res.json({ jwtToken });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// Log in
+app.post("/api/v1/productly/login", validInfo, async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (user.rows.length === 0) {
+      return res.status(401).json("User Doesn't exists!");
+    }
+
+    const validPassword = await bcrypt.compare(password, user.rows[0].password);
+
+    if (!validPassword) {
+      return res.status(401).json("Wrong Email or Password");
+    }
+    const jwtToken = jwtGenerator(user.rows[0].user_id);
+    return res.json({ jwtToken, user: user.rows[0] });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// Verify
+app.post("/verify", authorize, (req, res) => {
+  try {
+    res.json(true);
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+});
+
+// update user info
+app.put(
+  "/api/v1/productly/user/:id/edit-profile",
+  authorize,
+  async (req, res) => {
+    const {
+      first_name,
+      last_name,
+      bio_description,
+      display_picture,
+      cover_photo,
+    } = req.body;
+
+    const query = `UPDATE users SET 
+                      first_name = COALESCE($1, first_name),
+                      last_name = COALESCE($2, last_name),
+                      bio_description = COALESCE($3, bio_description),
+                      display_picture = COALESCE($4, display_picture),
+                      cover_photo = COALESCE($5, cover_photo)
+
+                    WHERE user_id = $6 returning *;`;
+
+    try {
+      const avatarFile = display_picture;
+
+      if (res.locals.user.id !== req.params.id) {
+        return res.status(403).json({ msg: "authorization denied" });
+      }
+
+      if (avatarFile !== null) {
+        const uploadedRespone = await cloudinary.uploader.upload(avatarFile, {
+          upload_preset: "productly_images",
+        });
+
+        const results = await db.query(query, [
+          first_name,
+          last_name,
+          bio_description,
+          uploadedRespone.url,
+          cover_photo,
+          req.params.id,
+        ]);
+
+        res.status(200).json({
+          status: "success",
+          data: {
+            user: results.rows[0],
+          },
+        });
+      } else {
+        const results = await db.query(query, [
+          first_name,
+          last_name,
+          bio_description,
+          display_picture,
+          cover_photo,
+          req.params.id,
+        ]);
+
+        res.status(200).json({
+          status: "success",
+          data: {
+            user: results.rows[0],
+          },
+        });
+      }
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+);
+
+// Get all Products
+app.get("/api/v1/productly/products", async (req, res) => {
+  try {
+    const products = await db.query(
+      "SELECT * FROM products LEFT JOIN (SELECT reviews_product_id, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews GROUP BY reviews_product_id) reviews on products.product_id = reviews.reviews_product_id;"
+    );
+
+    res.status(200).json({
+      status: "success",
+      results: products.rows.length,
+      data: {
+        products: products.rows,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// Get Popular Rated Products
+app.get("/api/v1/productly/products/top-rated", async (req, res) => {
+  try {
+    const products = await db.query(
+      `SELECT * FROM products
+      LEFT JOIN 
+          ( SELECT reviews_product_id, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews GROUP BY reviews_product_id ) 
+              reviews 
+          on products.product_id = reviews.reviews_product_id
+          ORDER BY average_rating DESC NULLS LAST;`
+    );
+
+    res.status(200).json({
+      status: "success",
+      results: products.rows.length,
+      data: {
+        products: products.rows,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// Get all Products created by fe1e8579-4ff7-4a61-bdb6-128342257308
+app.get("/api/v1/productly/products/:id", async (req, res) => {
+  try {
+    const products = await db.query(
+      "SELECT * FROM products WHERE user_id = $1",
+      [req.params.id]
+    );
+
+    res.status(200).json({
+      status: "success",
+      results: products.rows.length,
+      data: {
+        products: products.rows,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// Get single product
+app.get("/api/v1/productly/product/:id", async (req, res) => {
+  try {
+    const product = await db.query(
+      "SELECT * FROM products LEFT JOIN (SELECT reviews_product_id, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews GROUP BY reviews_product_id) reviews on products.product_id = reviews.reviews_product_id WHERE products.product_id = $1",
+      [req.params.id]
+    );
+
+    const productReviews = await db.query(
+      "SELECT * FROM reviews WHERE reviews_product_id = $1",
+      [req.params.id]
+    );
+
+    const productReviewCount = await db.query(
+      "select count(reviews_product_id) from reviews where reviews_product_id = $1 and review_message <> '';",
+      [req.params.id]
+    );
+
+    if (product.rows.length) {
+      res.status(200).json({
+        status: "success",
+        results: product.rows.length,
+        data: {
+          product: product.rows[0],
+          productReviews: productReviews.rows,
+          productReviewCount: productReviewCount.rows[0],
+        },
+      });
+    } else {
+      res.status(404).json({ message: "no data found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// Search product
+app.get("/api/v1/productly/products/search/:name", async (req, res) => {
+  try {
+    const products = await db.query(
+      `SELECT * FROM products
+      LEFT JOIN 
+          ( SELECT reviews_product_id, COUNT(*), TRUNC(AVG(rating),1) AS average_rating FROM reviews GROUP BY reviews_product_id ) 
+              reviews 
+          on products.product_id = reviews.reviews_product_id
+          WHERE LOWER(product_name) ~* $1;`,
+      [`^${req.params.name}.*`]
+    );
+
+    res.status(200).json({
+      status: "success",
+      products_results: products.rows.length,
+      data: {
+        products: products.rows,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// Get reviews of a single product
+app.get("/api/v1/productly/product/:id/reviews", async (req, res) => {
+  try {
+    const reviews = await db.query(
+      "SELECT * FROM reviews WHERE reviews_product_id = $1",
+      [req.params.id]
+    );
+
+    const averageRating = await db.query(
+      "SELECT TRUNC(AVG(rating),2) AS average_rating FROM reviews WHERE reviews_product_id = $1;",
+      [req.params.id]
+    );
+
+    res.status(200).json({
+      status: "success",
+      results: reviews.rows.length,
+      data: {
+        reviews: reviews.rows,
+        avgRating: averageRating.rows,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// Create a review
+app.post(
+  "/api/v1/productly/product/:user_id/:product_id/addReview",
+  async (req, res) => {
+    const { review_message, rating } = req.body;
+
+    try {
+      const review = await db.query(
+        "INSERT INTO reviews(reviews_product_id, user_id, review_message, rating) values($1, $2, $3, $4) returning *;",
+        [req.params.product_id, req.params.user_id, review_message, rating]
+      );
+
+      res.status(200).json({
+        status: "success",
+        results: review.rows.length,
+        data: {
+          review: review.rows,
+        },
+      });
+    } catch (error) {
+      res.status(400).json({ message: { error } });
+    }
+  }
+);
+
+// Update a review
+app.put("/api/v1/productly/product/:id/updateReview", async (req, res) => {
+  const { review_message, rating } = req.body;
+  try {
+    const results = await db.query(
+      "UPDATE reviews SET review_message = $1, rating = $2 WHERE review_id = $3 returning *;",
+      [review_message, rating, req.params.id]
+    );
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        review: results.rows[0],
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// delete a review
+app.delete("/api/v1/productly/product/:id/deleteReview", async (req, res) => {
+  try {
+    const removedReview = await db.query(
+      "DELETE FROM reviews WHERE review_id = $1 returning *;",
+      [req.params.id]
+    );
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        removedReview: removedReview.rows[0],
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// Create product
+app.post("/api/v1/productly/product/:id/addProduct", async (req, res) => {
+  const { product_name, product_description } = req.body;
+
+  try {
+    const newProduct = await db.query(
+      "INSERT INTO products(user_id, product_name, product_description) values($1, $2, $3) returning *;",
+      [req.params.id, product_name, product_description]
+    );
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        newProduct: newProduct.rows[0],
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// delete product
+app.delete("/api/v1/productly/product/:id/deleteProduct", async (req, res) => {
+  try {
+    const removedProduct = await db.query(
+      "DELETE FROM products WHERE product_id = $1 returning *",
+      [req.params.id]
+    );
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        removedProduct: removedProduct.rows[0],
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+// Update product
+app.put("/api/v1/productly/product/:id/updateProduct", async (req, res) => {
+  const { product_name, product_description } = req.body;
+  try {
+    const results = await db.query(
+      "UPDATE products SET product_name = $1, product_description = $2 WHERE product_id = $3 returning *",
+      [product_name, product_description, req.params.id]
+    );
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: { error } });
+  }
+});
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+  console.log(`Server is up and listening in PORT ${PORT}`);
+});
